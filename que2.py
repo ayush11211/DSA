cea392385a5b0f07392adc85374b5f15efb0007b
Ayush <ayushgyanchandani89@gmail.com>
2025-11-19 12:32:20 +0530

    que2 Count the no of digit in an Integer

---
+++ que2.py
@@ -0,0 +1,20 @@
+from math import *
+# Count the number of digits in an Integer
+# n = 5873
+# count = 0
+# digit = n
+# while digit > 0:
+#     last_digit = digit % 10
+#     digit = digit // 10
+#     count+=1
+# print(count)
+
+# One more way to solve this que using LOG way
+
+# def countd(d):
+#     print(int(log10(d)+1))
+#     return
+# countd(n)
+
+#Time and space Complexity
+#O(log10(N)) &&  O(1)
