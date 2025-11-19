f608a3b393f0ad60aace67a5c75a278f85b09d06
Ayush <ayushgyanchandani89@gmail.com>
2025-11-19 13:01:14 +0530

    Check if the no is Palindrome or Not

---
+++ que3.py
@@ -0,0 +1,15 @@
+# Check if the number is Palindrome or Not
+# n = 121
+# digit = n 
+# reverse = 0
+# while digit > 0:
+#     last_digit = digit % 10
+#     digit = digit // 10
+#     reverse = (reverse * 10)+ last_digit
+# if reverse == n:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+#Tc = O(log10(N))  SC = O(1)
