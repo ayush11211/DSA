602fb657b773fe0a0c7fc3d4e3afc250f4175bfa
Ayush <ayushgyanchandani89@gmail.com>
2025-11-25 00:24:11 +0530

    Check if the string is palindrome or not using recursion and loops

---
+++ que10.py
@@ -0,0 +1,30 @@
+#Check if the String is Palindrome or not
+
+
+#this is using recursion
+name = "nitin"
+
+# def pali(str,left,right):
+#     if left>=right:
+#         return True
+#     if str[left] != str[right]:
+#         return False
+#     return pali(str,left+1,right-1)
+
+# x = pali(name,0,len(name)-1)
+# print(x)
+
+
+#this is using loop
+# def pali():
+#     n = len(name)
+#     left = 0
+#     right = n-1
+#     while left<right:
+#         if name[left] != name[right]:
+#             return False
+#         left += 1
+#         right -= 1
+#     return True
+# x= pali()
+# print(x)
